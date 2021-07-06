f771012980c648160f49a19a696b5ab8dbbc46b3
missingXiong <k_x3@txstate.edu>
2021-07-05 22:55:29 -0500

    delete

--- Utility.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-
-int sendn(int fd, std::string &bufferout); // return 0, send buffer is full
-int recvn(int fd, std::string &bufferin);  // return 0, client has disconnected the connection
-int setnonblocking(int fd);
